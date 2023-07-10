20df8038c692344b8f361137349513f195ad8e57
Andrey-SPb1 <honor8x98@gmail.com>
2023-07-10 18:27:54 +0300

    WAR

--- src/main/java/maven/servlet/FirstServlet.java
+++ src/main/java/maven/servlet/FirstServlet.java
@@ -2,5 +2,11 @@ package maven.servlet;
 
 import jakarta.servlet.http.HttpServlet;
 
+import java.util.function.Supplier;
+
 public class FirstServlet extends HttpServlet {
+    public static void main(String[] args) {
+        Supplier<String> str = () -> "Hello World";
+        System.out.println(str.get());
+    }
 }
